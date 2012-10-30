eece19cfd8cd140183677d2788ef6eac50475f70
Igor Botov <ibotov@me.com>
2012-10-30 22:14:42 +0400

    paperclip fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -19,7 +19,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :category_id, :characteristics, :description, :image_url, :name, :photo
   has_attached_file :photo, :styles => { :small => "150x150>" },
-                    #:url  => "/assets/products/:id/:style/:basename.:extension",
+                    :url  => "/assets/products/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
   #has_attached_file :avatar,
   #                  :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
